971e47d813bc69e1555c7a1a29574d49cabaec82
DESKTOP-QQCIF0A\tak <hst85@nate.com>
2021-05-28 17:27:27 +0900

    imgui 적용

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -21,13 +21,18 @@ namespace Hazel {
 		void OnEvent(Event& e);
 
 		void PushLayer(Layer* layer);
-		void PushOverLayer(Layer* layer);
+		void PushOverLay(Layer* layer);
+
+		static inline Application& Get() { return *s_Instance; }
+		inline Window& GetWindow() { return *m_Window; }
 	private:
 		bool OnWindowClose(WindowCloseEvent& e);
 
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
+	private :
+		static Application* s_Instance;
 	};
 
 	// To be defined in CLIENT
